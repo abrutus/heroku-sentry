d75976d1eacb92c23dca9d60757c3c77c416dccf
Andre Brutus <abrutus@ubuntu.(none)>
2012-07-20 12:15:24 -0700

    Testing

--- sentry.conf.py
+++ sentry.conf.py
@@ -26,7 +26,7 @@ SENTRY_WEB_OPTIONS = {
 
 # For more information check Django's documentation:
 #  https://docs.djangoproject.com/en/1.3/topics/email/?from=olddocs#e-mail-backends
-"""
+
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
 EMAIL_HOST = 'localhost'
@@ -34,4 +34,4 @@ EMAIL_HOST_PASSWORD = ''
 EMAIL_HOST_USER = ''
 EMAIL_PORT = 25
 EMAIL_USE_TLS = False
-"""
+
